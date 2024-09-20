5d3589b0047af2e7cd1fce3a7406a6d66e15e574
Léo Bunel <leo.bunel@inrae.fr>
2024-09-20 13:17:10 +0200

    Add new options to autoamf.sh
    - Add -1 and -2 options to specify CNN1 and CNN2 networks
    - Add -t option for tile size (same as amf and amfbrowser)
    - Change threshold option to -s (frees up -t for consistency)
    - Remove readonly statements

--- amf/autoamf.sh
+++ amf/autoamf.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-USAGE='Usage: ./autoamf [-t THRESHOLD] FILE...'
-readonly USAGE
+usage='Usage: ./autoamf [-1 CNN1_NETWORK] [-2 CNN2_NETWORK] [-s THRESHOLD] [-t TILE_SIZE] FILE...'
 
-# Use the -t option to set the CNN2 threshold (0.5 by default).
-THRESHOLD=0.5
-while getopts 't:' flag; do
+# Set options defaults.
+cnn1_network="CNN1v2.h5"
+cnn2_network="CNN2v2.h5"
+threshold=0.5
+tile_size=126
+
+# Detect options (see Usage).
+while getopts '1:2:s:t:' flag; do
   case "${flag}" in
-    t) THRESHOLD="${OPTARG}" ;;
-    *) echo "${USAGE}" >&2 && exit 3 ;;
+    1) cnn1_network="${OPTARG}" ;;
+    2) cnn2_network="${OPTARG}" ;;
+    s) threshold="${OPTARG}" ;;
+    t) tile_size="${OPTARG}" ;;
+    *) echo "${usage}" >&2 && exit 3 ;;
   esac
 done
-readonly THRESHOLD
 
 # Verify that at least 1 file has been provided.
 if [ $(( $# - (OPTIND - 1) )) -lt 1 ]; then
-  echo "${USAGE}" >&2
+  echo "${usage}" >&2
   exit 4
 fi
 
 source amfenv/bin/activate
 
 # Predict fungal colonisation (CNN1) on a bunch of JPEG or TIFF images.
-./amf predict --network CNN1v2.h5 "${@:$OPTIND}"
+./amf predict --network "${cnn1_network}" --tile_size "${tile_size}" "${@:$OPTIND}"
 
 # Convert CNN1 predictions to annotations.
 ./amf convert --CNN1 "${@:$OPTIND}"
 
 # Predict intraradical structures (CNN2) on the same images.
-./amf predict --network CNN2v2.h5 "${@:$OPTIND}"
+./amf predict --network "${cnn2_network}" --tile_size "${tile_size}" "${@:$OPTIND}"
 
 # Convert CNN2 predictions to annotations using the specified threshold.
-./amf convert --CNN2 --threshold "${THRESHOLD}" "${@:$OPTIND}"
+./amf convert --CNN2 --threshold "${threshold}" "${@:$OPTIND}"
 
 deactivate
